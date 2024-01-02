557117c7f9200c7b9a647558c0b135c98b2db886
garroc <clement.garro@etu.umontpellier.fr>
2024-01-02 01:49:06 +0100

    fix: color

--- src/components/components.js
+++ src/components/components.js
@@ -3,6 +3,5 @@ import Loader from "./Loader.jsx";
 import HomeInfo from "./HomeInfo.jsx";
 import CTA from "./CTA.jsx";
 import Footer from "./Footer.jsx";
-import CreateMesh from "./CreateMesh.jsx";
 
-export { Alert, Loader, HomeInfo, CTA, Footer, CreateMesh };
+export { Alert, Loader, HomeInfo, CTA, Footer };

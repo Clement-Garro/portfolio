5e69ec024cba0d86382610da937096536f040802
garroc <clement.garro@etu.umontpellier.fr>
2024-03-14 20:12:25 +0100

    update

--- src/pages/projectPages/fate-of-the-same.jsx
+++ src/pages/projectPages/fate-of-the-same.jsx
@@ -9,39 +9,90 @@ const CodeGameJam = () => {
                         Fate of the Same
                     </span>
                 </h1>
-                <p><strong>Date: </strong>Janvier 2023 - Janvier 2023</p>
+                <p><strong>Date: </strong>Janvier 2023</p>
             </div>
 
             <div className="pb-5 object-contain">
                 <h2 className="subhead-text pb-4">
                     <span className="gradient_text drop-shadow font-semibold">
-                        À Propos
+                        Contexte
                     </span>
                 </h2>
                 <p>
-                    Jeu vidéo conçu lors de la 7ᵉ édition du Code Game Jam 2023 à l'IUT de Montpellier-Sète. Réalisé sur
-                    Unity, ce jeu captivant résulte d'une expérience intense de création en seulement 30 heures.
+                    Durant la CodeGameJam2023, notre équipe s'est lancée dans un défi ambitieux sur Unity en 2D : la
+                    création de Fate of the Same, un jeu d'aventure immersif où les joueurs explorent un monde où le
+                    temps et l'identité s'entremêlent. Le jeu est animé avec des sprites dynamiques, notamment pour les
+                    attaques et les capacités spéciales, donnant vie à l'action et à l'aventure.
                 </p>
             </div>
 
             <div className="pb-5">
                 <h2 className="subhead-text pb-4">
                     <span className="gradient_text drop-shadow font-semibold">
-                        Compétences
+                        Histoire
+                    </span>
+                </h2>
+                <p>
+                    Dans Fate of the Same, les joueurs incarnent un héros apparaissant au cœur d'une plateforme céleste,
+                    entouré d'étoiles. Leur mission ? Défaire Chronos, le maître du temps. Pour y parvenir, ils doivent
+                    utiliser des téléporteurs pour explorer la carte et affronter des défis variés. Des forêts
+                    mystérieuses aux déserts arides, chaque zone révèle un fragment de l'histoire de Chronos, jusqu'à un
+                    affrontement final surprenant.
+                    Le récit prend une tournure inattendue lorsque le joueur découvre que Chronos est en réalité une
+                    version future de lui-même, piégée dans une boucle temporelle. La confrontation finale soulève des
+                    questions sur l'identité et le destin, révélant une dualité intrigante entre le héros et son
+                    antagoniste.
+                </p>
+            </div>
+
+            <div className="pb-5">
+                <h2 className="subhead-text pb-4">
+                    <span className="gradient_text drop-shadow font-semibold">
+                        Gameplay
+                    </span>
+                </h2>
+                <p>
+                    Fate of the Same propose un gameplay dynamique centré sur la gestion du temps. Les joueurs doivent
+                    affronter des ennemis et résoudre des énigmes tout en surveillant une barre de temps. Chaque
+                    élimination de monstre permet de récupérer du pouvoir temporel, tandis qu'une capacité spéciale
+                    permet de figer le temps temporairement, au prix d'une consommation de ressources temporelles.
+                    Bien que nous n'ayons pas eu le temps d'intégrer toutes nos idées, notre intention était d'inclure
+                    la possibilité d'envoyer des projectiles temporels, offrant ainsi une mécanique de combat encore
+                    plus dynamique et stratégique.
+                </p>
+            </div>
+
+            <div className="pb-5">
+                <h2 className="subhead-text pb-4">
+                    <span className="gradient_text drop-shadow font-semibold">
+                        Expérience Personnelle
+                    </span>
+                </h2>
+                <p>
+                    Participer à la CodeGameJam2023 a été une aventure passionnante pour moi, combinant mes intérêts
+                    pour la programmation et les jeux vidéo. Travailler sur Fate of the Same m'a permis de repousser mes
+                    limites et d'explorer de nouvelles idées de gameplay en équipe.
+                    Malgré les défis techniques et les contraintes de temps, je suis fier du résultat que notre équipe a
+                    accompli. Cette expérience collaborative m'a permis de développer mes compétences en programmation
+                    tout en partageant une passion commune pour la création de jeux vidéo.
+                    Fate of the Same restera pour moi une expérience mémorable et stimulante, et j'espère pouvoir
+                    continuer à explorer de nouvelles idées et à collaborer avec des talents créatifs dans le futur.
+                </p>
+            </div>
+            <div className="pb-5">
+                <h2 className="subhead-text pb-4">
+                    <span className="gradient_text drop-shadow font-semibold">
+                        Compétences Apportées
                     </span>
                 </h2>
                 <ul>
-                    <li>Développement de Jeux Vidéo sur Unity</li>
-                    <li>Gestion du Temps</li>
-                    <li>Travail en Équipe</li>
-                    <li>Créativité et Résolution de Problèmes</li>
-                    <li>Adaptabilité</li>
-                    <li>Intégration de Compétences Multidisciplinaires</li>
-                    <li>Présentation de Projets</li>
-                    <li>Expérience dans les Événements de Game Jam</li>
+                    <li>Travail d'équipe</li>
+                    <li>Créativité</li>
+                    <li>Conception de jeux vidéo</li>
+                    <li>Programmation en équipe</li>
+                    <li>Unity</li>
                 </ul>
             </div>
-
             <div className="pb-5">
                 <h2 className="subhead-text pb-4">
                     <span className="gradient_text drop-shadow font-semibold">
@@ -50,15 +101,29 @@ const CodeGameJam = () => {
                 </h2>
                 <ul>
                     <li>Daniil Hirchyts - 1er BUT chez IUT de Montpellier-Sète</li>
-                    <li>Giovanni Gozzo - 1er Étudiant(e) à IUT MONTPELLIER-SETE</li>
-                    <li>Soren Starck - 1er Étudiant(e) à IUT MONTPELLIER-SETE en Informatique parcours Réalisation
-                        d'applications : conception, développement, validation
-                    </li>
-                    <li>Norman François - 1er Étudiant(e) à IUT MONTPELLIER-SETE</li>
+                    <li>Giovanni Gozzo - 1er BUT chez IUT de Montpellier-Sète</li>
+                    <li>Norman Francois - 1er BUT chez IUT de Montpellier-Sète</li>
+                    <li>Soren Stark - 1er BUT chez IUT de Montpellier-Sète</li>
+                    <li>Rémi ... - 1er BUT chez IUT de Montpellier-Sète</li>
                 </ul>
             </div>
         </section>
     );
 };
+
+
+// Gameplay :
+//
+//     Fate of the Same propose un gameplay dynamique centré sur la gestion du temps. Les joueurs doivent affronter des ennemis et résoudre des énigmes tout en surveillant une barre de temps. Chaque élimination de monstre permet de récupérer du pouvoir temporel, tandis qu'une capacité spéciale permet de figer le temps temporairement, au prix d'une consommation de ressources temporelles.
+//
+//     Bien que nous n'ayons pas eu le temps d'intégrer toutes nos idées, notre intention était d'inclure la possibilité d'envoyer des projectiles temporels, offrant ainsi une mécanique de combat encore plus dynamique et stratégique.
+//
+//     Expérience Personnelle :
+//
+//     Participer à la CodeGameJam2024 a été une aventure passionnante pour moi, combinant mes intérêts pour la programmation et les jeux vidéo. Travailler sur Fate of the Same m'a permis de repousser mes limites et d'explorer de nouvelles idées de gameplay en équipe.
+//
+//     Malgré les défis techniques et les contraintes de temps, je suis fier du résultat que notre équipe a accompli. Cette expérience collaborative m'a permis de développer mes compétences en programmation tout en partageant une passion commune pour la création de jeux vidéo.
+//
+// Fate of the Same restera pour moi une expérience mémorable et stimulante, et j'espère pouvoir continuer à explorer de nouvelles idées et à collaborer avec des talents créatifs dans le futur.
 
 export default CodeGameJam;
---
+++ src/pages/projectPages/shadow-of-memory.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+
+const shadowOfMemory = () => {
+    return (
+        <section className="project-info-box">
+            <div className="pb-5">
+                <h1 className="head-text">
+                    <span className="gradient_text drop-shadow font-semibold">
+                        Shadow of Memory
+                    </span>
+                </h1>
+                <p><strong>Date: </strong>Janvier 2024</p>
+            </div>
+
+            <div className="pb-5 object-contain">
+                <h2 className="subhead-text pb-4">
+                    <span className="gradient_text drop-shadow font-semibold">
+                        Contexte
+                    </span>
+                </h2>
+                <p>
+                    Durant la CodeGameJam2024, notre équipe s'est lancée dans une aventure audacieuse sur Unreal Engine
+                    5 : la création de Shadow of Memory, un jeu vidéo en 3D à la troisième personne qui transporte les
+                    joueurs dans un monde mystérieux où le temps et la mémoire s'entremêlent.
+                </p>
+            </div>
+
+            <div className="pb-5">
+                <h2 className="subhead-text pb-4">
+                    <span className="gradient_text drop-shadow font-semibold">
+                        Histoire
+                    </span>
+                </h2>
+                <p>
+                    Dans Shadow of Memory, les joueurs incarnent un être éthéré dont l'existence est en danger d'être
+                    engloutie par l'oubli. Pour survivre, ils doivent reconstruire leur propre légende en explorant des
+                    contrées mystiques et en interagissant avec des esprits du passé. Chaque choix du joueur influence
+                    le destin de l'éthéré : des actes de compassion le transforment en une légende mémorable, tandis que
+                    succomber à l'indifférence entraîne une disparition totale.
+                    La dualité entre devenir une figure mythique ou s'évanouir dans l'obscurité de l'oubli est au cœur
+                    de l'aventure. Chaque action teinte le récit dans cette aventure où la frontière entre la lumière et
+                    l'ombre, la légende et l'oubli, est aussi fragile que l'essence même du protagoniste éthéré.
+                </p>
+            </div>
+
+            <div className="pb-5">
+                <h2 className="subhead-text pb-4">
+                    <span className="gradient_text drop-shadow font-semibold">
+                        Gameplay
+                    </span>
+                </h2>
+                <p>
+                    Shadow of Memory propose un gameplay simple mais captivant, malgré les contraintes de temps de la
+                    jam. Les joueurs explorent un monde mystique en 3D, où ils peuvent interagir avec des personnages
+                    non-joueurs et des éléments environnementaux pour avancer dans l'histoire.
+                    Bien que nous n'ayons pas pu intégrer toutes nos idées dans le jeu en 24 heures, nous avons réussi à
+                    mettre en place un système de déplacement fluide et une mécanique de score qui représente le niveau
+                    de légende du personnage principal. En visitant des villages dispersés dans une vaste carte 3D, les
+                    joueurs peuvent hantent certaines maisons et accomplir des actions héroïques pour augmenter leur
+                    score de légende.
+                </p>
+            </div>
+            <div className="pb-5">
+                <h2 className="subhead-text pb-4">
+                    <span className="gradient_text drop-shadow font-semibold">
+                        Expérience Personnelle
+                    </span>
+                </h2>
+                <p>
+                    La CodeGameJam2024 a été une expérience passionnante pour moi, car elle a réuni mes deux passions
+                    professionnelles et personnelles : les jeux vidéo et la programmation. Travailler sur Shadow of
+                    Memory m'a permis de combiner mes compétences techniques en programmation avec ma créativité et ma
+                    passion pour les jeux vidéo.
+
+                    Même si nous avons dû faire face à des défis et des contraintes de temps, j'ai trouvé le processus
+                    de développement du jeu extrêmement gratifiant. Collaborer avec une équipe passionnée pour donner
+                    vie à notre vision a été une expérience enrichissante et inspirante.
+
+                    Je suis reconnaissant d'avoir eu l'opportunité de participer à cette jam et je suis fier du résultat
+                    que notre équipe a pu accomplir en si peu de temps. Shadow of Memory restera pour moi un souvenir
+                    précieux et une source d'inspiration pour les projets futurs.
+
+                </p>
+            </div>
+            <div className="pb-5" key="videoYoutube">
+                <h2 className="subhead-text pb-4">
+                        <span className="gradient_text drop-shadow font-semibold">
+                            Vidéo de Gameplay
+                        </span>
+                </h2>
+                <p>
+                    Pour des raisons de droits d'auteur sur la music, le son a été retiré de la vidéo.
+                </p>
+                <div className="pb-5">
+                    <iframe
+                        className="w-full h-96 object-cover rounded-lg shadow-lg"
+                        src="https://youtube.com/embed/OoIJkiQOXEQ"
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+                </div>
+            </div>
+            <div className="pb-5">
+                <h2 className="subhead-text pb-4">
+                    <span className="gradient_text drop-shadow font-semibold">
+                        Compétences Apportées
+                    </span>
+                </h2>
+                <ul>
+                    <li>Travail d'équipe</li>
+                    <li>Créativité</li>
+                    <li>Conception de jeux vidéo</li>
+                    <li>Programmation en équipe</li>
+                    <li>UnrealEngine5</li>
+                    <li>Game Design</li>
+                    <li>3D Modeling</li>
+                    <li>World Building</li>
+                    <li>Level Design</li>
+                    <li>Material Design</li>
+                </ul>
+            </div>
+            <div className="pb-5">
+                <h2 className="subhead-text pb-4">
+                    <span className="gradient_text drop-shadow font-semibold">
+                        Contributeurs
+                    </span>
+                </h2>
+                <ul>
+                    <li>Kylian R. - 1er BUT chez IUT de Montpellier-Sète</li>
+                    <li>Galdric D. - 1er BUT chez IUT de Montpellier-Sète</li>
+                    <li>Nicolas L. - 1er BUT chez IUT de Montpellier-Sète</li>
+                </ul>
+            </div>
+        </section>
+    );
+}
+
+export default shadowOfMemory;
